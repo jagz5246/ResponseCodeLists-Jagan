43ecaedfe151ca60113dd32b53b349710843e7d1
Jagan G Mohan <jagz5246@gmail.com>
2025-03-02 19:40:45 +0530

    Testing build

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -2,26 +2,11 @@ import * as React from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import Slide from '@mui/material/Slide';
 
-function SlideTransition(props) {
-  return <Slide {...props} direction="up" />;
-}
-
-function GrowTransition(props) {
-  return <Grow {...props} />;
-}
-
 export default function Toast({message}) {
   const [state, setState] = React.useState({
     open: false,
     Transition: Slide,
   });
-
-  // const handleClick = (Transition) => () => {
-  //   setState({
-  //     open: true,
-  //     Transition,
-  //   });
-  // };
 
   const handleClose = () => {
     setState({
